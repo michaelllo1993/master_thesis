a35030a0d899aa101a087f4aec37d2d05d90163e
Michał <stolarczyk.michal93@gmail.com>
2016-12-19 21:31:48 +0100

    Further work on codon changes within leucine
    Former-commit-id: c3d2e3a2b2562deed307b837adf7b24fdb3b0b0b

--- analiza_kodonow/compare_regions_codon_usage.R
+++ analiza_kodonow/compare_regions_codon_usage.R
@@ -3,15 +3,11 @@ load("compare_region/data.RData")
 load("compare_region/with_protein.RData")
 load("compare_region/with_sigp.RData")
 # all ---------------------------------------------------------------------
-
-
 a = matrix(data = NaN,nrow = length(cDNA$cDNA_homo_sapiens$SEQUENCE),ncol = length(leucine_codons));
 codons_count_protein = list(a,a,a,a,a,a,a,a,a);
 leucine_codons = c("TTA","TTG","CTT","CTC","CTA","CTG");
-
 for(org in seq(1,9,1)){
   for (i in seq(1,length(cDNA[[org]]$SEQUENCE))) {
-
     if (!length(sigp[[org]]$cDNA_ID[which(sigp[[org]]$cDNA_ID == cDNA[[org]]$cDNA_ID[i])]) == 0){
       sp_end = sigp[[org]]$SP_END[which(sigp[[org]]$cDNA_ID == cDNA[[org]]$cDNA_ID[i])]
     }
@@ -25,13 +21,10 @@ for(org in seq(1,9,1)){
   }
   print(paste("ORGANISM",org,"done"))
 }
-
 # sigp --------------------------------------------------------------------
-
 a = matrix(data = NaN,nrow = length(sigp$sigp_ALL_homo_sapiens$SEQ),ncol = length(leucine_codons));
 codons_count_sigp = list(a,a,a,a,a,a,a,a,a);
 leucine_codons = c("TTA","TTG","CTT","CTC","CTA","CTG");
-
 for(org in seq(1,9,1)) {
   for (i in seq(1,length(sigp[[org]]$SEQ))) {
     if (!length(sigpL[[org]]$cDNA_ID[which(sigpL[[org]]$cDNA_ID == sigp[[org]]$cDNA_ID[i])]) == 0){
@@ -52,13 +45,10 @@ for(org in seq(1,9,1)) {
   }
   print(paste("ORGANISM",org,"done"))
 }
-
 # L-SAAR ------------------------------------------------------------------
-
 a = matrix(data = NaN,nrow = length(sigpL$sigpL_ALL_homo_sapiens$SEQ),ncol = length(leucine_codons));
 codons_count_lsaar = list(a,a,a,a,a,a,a,a,a);
 leucine_codons = c("TTA","TTG","CTT","CTC","CTA","CTG");
-
 for(org in seq(1,9,1)) {
   for (i in seq(1,length(sigpL[[org]]$SEQ))) {
       for (codon in seq(1,length(leucine_codons),by = 1)) {
@@ -70,9 +60,7 @@ for(org in seq(1,9,1)) {
   }
   print(paste("ORGANISM",org,"done"))
 }
-
 # data manipualtion -------------------------------------------------------
-
 for (i in seq(1,length(codons_count_lsaar),1)){
   codons_count_lsaar[[i]] = codons_count_lsaar[[i]][seq(1,length(which(!is.na(codons_count_lsaar[[i]][,1]))),1),]
   codons_count_protein[[i]] = codons_count_protein[[i]][seq(1,length(which(!is.na(codons_count_protein[[i]][,1]))),1),]
@@ -84,38 +72,30 @@ for (i in seq(1,length(codons_count_lsaar),1)){
 names(codons_count_lsaar)=prot_names
 names(codons_count_protein)=prot_names
 names(codons_count_sigp)=prot_names
-
 # data into matrices ------------------------------------------------------
-
 codons_count_lsaar_fraction = matrix(NaN,nrow = length(codons_count_lsaar) ,ncol = length(leucine_codons))
 for (org in seq(1,length(codons_count_lsaar),1)){
   for (cod in seq(1,length(leucine_codons),1)){
     codons_count_lsaar_fraction[org,cod] = sum(codons_count_lsaar[[org]][,cod],na.rm = T)/sum(codons_count_lsaar[[org]],na.rm = T)
   }
 }
-
 codons_count_sigp_fraction = matrix(NaN,nrow = length(codons_count_sigp) ,ncol = length(leucine_codons))
 for (org in seq(1,length(codons_count_sigp),1)){
   for (cod in seq(1,length(leucine_codons),1)){
     codons_count_sigp_fraction[org,cod] = sum(codons_count_sigp[[org]][,cod],na.rm = T)/sum(codons_count_sigp[[org]],na.rm = T)
   }
 }
-
 codons_count_protein_fraction = matrix(NaN,nrow = length(codons_count_protein) ,ncol = length(leucine_codons))
 for (org in seq(1,length(codons_count_protein),1)) {
   for (cod in seq(1,length(leucine_codons),1)) {
     codons_count_protein_fraction[org,cod] = sum(codons_count_protein[[org]][,cod],na.rm = T)/sum(codons_count_protein[[org]],na.rm = T)
   }
 }
-
 rownames(codons_count_protein_fraction)=prot_names
 rownames(codons_count_sigp_fraction)=prot_names
 rownames(codons_count_lsaar_fraction)=prot_names
-
 colnames(codons_count_protein_fraction)=leucine_codons
 colnames(codons_count_sigp_fraction)=leucine_codons
 colnames(codons_count_lsaar_fraction)=leucine_codons
-
 # saving environment ------------------------------------------------------
-
 save.image(file = paste(sep = "",date(),".RData"))
--- analiza_kodonow/within_leucine/codon_changes_within_leucine.R
+++ analiza_kodonow/within_leucine/codon_changes_within_leucine.R
@@ -1,26 +1,62 @@
-library(Biostrings);library(seqinr)
-setwd(dir = "/home/mstolarczyk/Uczelnia/MGR/praca_magisterska/analiza_kodonow/within_leucine/")
-homo_sapiens_data = as.matrix(read.csv(file = "homo_sapiens_revtrans.csv",header = F))
-a = matrix(NaN,nrow = dim(homo_sapiens_data)[1],ncol = dim(homo_sapiens_data)[2])
-homo_sapiens = list(a,a,a,a,a,a,a,a);
-homo_sapiens[[6]] = homo_sapiens_data[grep(pattern = "ENSPTRP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[4]] = homo_sapiens_data[grep(pattern = "ENSMMUP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[3]] = homo_sapiens_data[grep(pattern = "ENSGGOP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[5]] = homo_sapiens_data[grep(pattern = "ENSMUSP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[7]] = homo_sapiens_data[grep(pattern = "ENSRNOP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[1]] = homo_sapiens_data[grep(pattern = "ENSBTAP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[2]] = homo_sapiens_data[grep(pattern = "ENSGALP[0-9]+",homo_sapiens_data[,3],perl = T),]
-homo_sapiens[[8]] = homo_sapiens_data[grep(pattern = "ENSXETP[0-9]+",homo_sapiens_data[,3],perl = T),]
+#USAGE: RScprit name organism_of_inerest_name organism_of_interest_exclusive_code
 
-names(homo_sapiens) = prot_names[-which(prot_names == "homo_sapiens")]
+wd = getwd()
+require(seqinr)
+require(Biostrings)
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)!=3){
+  print("Wrong number of arguments passed!")
+  stop()
+}
 
+organism_of_interest_name = args[1]
+organisms = c("bos_taurus","gallus_gallus","gorilla_gorilla","homo_sapiens","macaca_mulatta","mus_musculus","pan_troglodytes","rattus_norvegicus","xenopus_tropicalis")
+tmp_organisms = organisms[-which(prot_names == organism_of_interest_name)]
+ensembl_exclusive_codes = c("ENSBTAP","ENSGALP","ENSGGOP","ENSP","ENSMMUP","ENSMUSP","ENSPTRP","ENSRNOP","ENSXETP");
+tmp_codes = ensembl_exclusive_codes[-which(ensembl_exclusive_codes == args[2])]
 
-for (org in seq(1,length(homo_sapiens),by = 1)){
-  for (i in seq(1, dim(homo_sapiens)[1],by = 1)){
-    for (l in seq(1, length(leucine_codons),by = 1)){
-      human_codons = strsplit(homo_sapiens[i,2], "(?<=.{3})", perl = TRUE)[[1]]
-      human_L_indices = which(human_codons == leucine_codons[l])
-      other_codons = strsplit(homo_sapiens[i,4], "(?<=.{3})", perl = TRUE)[[1]][human_L_indices]
+# Reading the data (output from run_revtrans.pl) --------------------------
+organism_of_interest_data = as.matrix(read.csv(file = paste(organism_of_interest_name,"_revtrans.csv",sep = ""),header = F))
+organism_of_interest = lapply(1:9,function(x) matrix(NaN,nrow = dim(organism_of_interest_data)[1],ncol = dim(organism_of_interest_data)[2]));
+for (org in seq(1,length(tmp_codes),by = 1)){
+  reg_ex = paste(tmp_codes[org],"[0-9]+",sep = "");
+  organism_of_interest[[org]] = organism_of_interest_data[grep(pattern = reg_ex ,organism_of_interest_data[,3],perl = T),]
+}
+names(organism_of_interest) = tmp_organisms
+
+# Loop constructing thee lists with codons on leucine codons positions in organism of interest cDNA sequences --------
+OoI = lapply(1:length(v), function(x) lapply(1:length(leucine_codons), function(x) NaN))
+other_codons = c();
+for (org in seq(1,length(v),by = 1)){
+  print(paste("Processing orgsnism: ",sep = "", current_names[org]))
+  for (l in seq(1, length(leucine_codons),by = 1)){
+    print(paste("Codon: ",sep = "",leucine_codons[l]))
+    for (i in seq(1, dim(v[[org]])[1],by = 1)){
+      organism_of_interest_codons = strsplit(organism_of_interest[[org]][i,2], "(?<=.{3})", perl = TRUE)[[1]]
+      organism_of_interest_L_indices = which(organism_of_interest_codons == leucine_codons[l])
+      other_codons_tmp = strsplit(organism_of_interest[[org]][i,4], "(?<=.{3})", perl = TRUE)[[1]][organism_of_interestL_indices]
+      other_codons = append(other_codons,other_codons_tmp)
     }
+    OoI[[org]][[l]] = table(other_codons)
+    other_codons = c();
   }
+}
+names(OoI) = current_names
+for (org in seq(1,length(OoI),by = 1)){
+  names(OoI[[org]]) = leucine_codons
+}
+
+# Saving codons of other organisms into matrix - NEEDS CORRECTION ---------------------------
+
+
+output = c()
+for (org in seq(1,length(OoI),by = 1)){
+  for (l in seq(1,length(leucine_codons),by = 1)){
+    row.names(output) = output[,1]
+    output = merge(output,as.matrix(OoI[[org]][[l]]),by="row.names",all=T)
+  }
+  row_names = output[,1]
+  output = output[,-c(1:length(leucine_codons))]
+  rownames(output) = row_names
+  colnames(output) = leucine_codons
 }
