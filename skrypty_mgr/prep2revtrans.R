32ff2d12c24f6b1fbe216af3a400338893d064e9
Michał <stolarczyk.michal93@gmail.com>
2017-03-31 18:52:09 +0200

    Dalsza praca nad prep2revtrans.R szybciej dziala, ale jeszcze wymaga pracy
    Former-commit-id: d2c0789674ee0612681134c5ce8eb8259bacb235

--- skrypty_mgr/prep2revtrans.R
+++ skrypty_mgr/prep2revtrans.R
@@ -11,33 +11,29 @@ for (k in 1:length(myFiles)) {
 cDNA_names[k] = strsplit(myFiles,"[.]")[[k]][1];
 names(cDNA) = cDNA_names;
 
-#import packages
-library(foreach)
-library(doParallel)
 
-# prepare cDNA sequences (0-69*3) to RevTrans -----------------------------
-#setup parallel backend to use 3processors
-cl <- makeCluster(3)
-registerDoParallel(cl)
-
-#start timecDN
-strt <- Sys.time()
-
-########### Split START and STOP values - time consuming ##########
 for (org in seq(1,length(cDNA))){
-  for(s in seq(1,length(cDNA[[org]][[3]]))) {
-    if (length(grep(";",cDNA[[org]][[3]][s])) == 1) {
-      cDNA[[org]][[3]][s] = strsplit(cDNA[[org]][[3]][s][[1]],";");#split by semicolon
-      cDNA[[org]][[4]][s] = strsplit(cDNA[[org]][[4]][s][[1]],";");#split by semicolon
-    }
-    else{
-      cDNA[[org]][[3]][s] = cDNA[[org]][[3]][s][[1]];
-      cDNA[[org]][[4]][s] = cDNA[[org]][[4]][s][[1]];
-    }
-  }
-  print(paste("cDNA data processed for:",cDNA_names[org]))
+    cDNA[[org]][[3]] = lapply(cDNA[[org]][[3]], function(x) strsplit(x = x, split = ";")[[1]]);#split by semicolon
+    cDNA[[org]][[4]] = lapply(cDNA[[org]][[4]], function(x) strsplit(x = x, split = ";")[[1]])
+    print(paste("cDNA data processed for:",cDNA_names[org]))
 }
 
+trimm2coding = function(cDNA){
+  return(c2s(s2c(cDNA[[5]])[seq(min(as.numeric(unlist(cDNA[[3]]))),max(as.numeric(unlist(cDNA[[4]]))),by = 1)]))
+}
+a = c();
+cDNA_test = list(a,a,a,a,a,a,a,a,a);
+for(org in seq(1,length(cDNA))){
+  for (i in seq(1,length(cDNA[[org]]$STOP))){
+    vec=c();
+    starting=min(as.numeric(unlist(cDNA[[org]]$START[i])))#coding sequence start
+    stopping=max(as.numeric(unlist(cDNA[[org]]$STOP[i])))#coding sequence end
+    vec = seq(starting,stopping,by = 1)#numbers of coding nucleotides
+    cseq=c2s(s2c(cDNA[[org]]$SEQUENCE[i])[vec]); #proper coding sequence
+    cDNA[[org]]$SEQUENCE[i] = cseq
+  }
+  print(paste("cDNA data processed for",cDNA_names[org]))
+}
 
 a = c();
 prep2revtrans = list(a,a,a,a,a,a,a,a,a);
@@ -51,7 +47,7 @@ for(org in 1:length(cDNA)){
       prep2revtrans[[org]][i,2] = prep2revtrans[[org]][i,2]$SEQUENCE
     }
   }
-  names(prep2revtrans) = prot_names
+  names(prep2revtrans) = cDNA_names
   for (i in seq(1, length(prep2revtrans),1)){
     write.csv(row.names = F,x = prep2revtrans[[i]],file = paste(getwd(),"/","prep2revtrans","_",cDNA_names[i],".csv",sep = ""))
   }
