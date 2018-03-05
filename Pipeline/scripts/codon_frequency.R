3d4599e85de914591888e780aa034bd9826bfa37
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-03-05 17:01:07 -0500

    Codon frequency accounts for SAARs and SPs now

--- Pipeline/scripts/codon_frequency.R
+++ Pipeline/scripts/codon_frequency.R
@@ -101,7 +101,7 @@ for (org in seq_len(ncol(orthologues))) {
         # Get only the translated nucleotides
         translated = cdna[vec];
         # Trim the sequence to the preticted length of signal peptide * 3
-        translated = translated[1:3*sp_length]
+        translated = translated[1:(sp_length*3-1)]
         # In case of faulty ("N") nucleotide - replace it with a random one
         translated[which(translated == "N")] = nucleotides[sample(1:4, 1)]
         # Sequence to codons and get the frequencies
@@ -145,8 +145,8 @@ for (org in seq_len(ncol(orthologues))) {
         }
         # Get only the translated nucleotides
         translated = cdna[vec];
-        # Trim the sequence to the preticted length of signal peptide * 3
-        translated = translated[3*sp_length:length(translated)]
+        # Cut the signal peptide from the sequence
+        translated = translated[(1+3*sp_length):length(translated)]
         # In case of faulty ("N") nucleotide - replace it with a random one
         translated[which(translated == "N")] = nucleotides[sample(1:4, 1)]
         # Sequence to codons and get the frequencies
@@ -161,6 +161,52 @@ for (org in seq_len(ncol(orthologues))) {
   }
   # Write the result to .csv file
   write.csv(result,file = paste(sep = "",getwd(),"/codon_frequency/",organisms_names[org],"_codon_frequency_excluding_SP.csv"));
+}
+
+
+# Within SAARs ------------------------------------------------------------
+
+for (org in seq_len(ncol(orthologues))) {
+  result = c();
+  # Prepare empty result matrix
+  result = matrix("",nrow(orthologues) * 2,length(unit_codons)+1);
+  for (i in seq_len(nrow(orthologues))) {
+    # Find cDNA of interest
+    which_cDNA = which(cDNA[[org]]$ID == orthologues[i,org])
+    if(length(which_cDNA)>0){
+      # Get the sequence
+      cdna = s2c(cDNA[[org]]$SEQUENCE[which_cDNA])
+      cdna_id = cDNA[[org]]$cDNA_ID[which_cDNA]
+      # Get the start point and length of SAAR in the predicted signal peptide and check if it exists
+      saar_start = SAAR[[org]][which(SAAR[[org]][,2] == cdna_id),4]
+      saar_length = SAAR[[org]][which(SAAR[[org]][,2] == cdna_id),3]
+      if(length(saar_start)>0){
+        # Get the starting and stopping positions of the translated sequence 
+        codon_start = c(); codon_start = cDNA[[org]]$START[which_cDNA][[1]]
+        codon_stop = c(); codon_stop = cDNA[[org]]$STOP[which_cDNA][[1]]
+        vec = c()
+        # Concatenate all translated region
+        for (j in seq_len(length(codon_start))) {
+          vec = append(x = vec,values = seq(codon_start[j],codon_stop[j],by = 1));
+        }
+        # Get only the translated nucleotides
+        translated = cdna[vec];
+        # Cut the signal peptide from the sequence
+        translated = translated[(3*saar_start+1):(3*saar_start+1+(saar_length*3))]
+        # In case of faulty ("N") nucleotide - replace it with a random one
+        translated[which(translated == "N")] = nucleotides[sample(1:4, 1)]
+        # Sequence to codons and get the frequencies
+        codons_freq = table(as.character(codons(DNAString(c2s(translated)))));
+        # Just unit of interest codons codons
+        avail_unit_codons_freq = codons_freq[unit_codons];
+        # Combine results into a matrix
+        result[(i * 2) - 1,] = append(names(avail_unit_codons_freq),cDNA[[org]]$cDNA_ID[which_cDNA])
+        result[i * 2,] = append(as.vector(avail_unit_codons_freq),saar_length)
+      }
+    }
+  }
+  # Write the result to .csv file
+  write.csv(result,file = paste(sep = "",getwd(),"/codon_frequency/",organisms_names[org],"_codon_frequency_SAAR.csv"));
 }
 
   
--- Pipeline/scripts/readData.R
+++ Pipeline/scripts/readData.R
@@ -36,14 +36,14 @@ names(mapper) = map_names
 # Read the cDNA sequences -------------------------------------------------
 
 setwd(paste(wd, "/data/cDNAsequences/", sep = ""))
-myFiles <- list.files(pattern = "*.csv")
+myFiles2 <- list.files(pattern = "*.csv")
 cDNA = list()
 cDNA_names = c()
-for (k in 1:length(myFiles)) {
+for (k in 1:length(myFiles2)) {
   cDNA_names[k] = strsplit(myFiles, "[.]")[[k]][1]
-  cDNA[[k]] = read.csv(myFiles[k], stringsAsFactors = F)
+  cDNA[[k]] = read.csv(myFiles2[k], stringsAsFactors = F)
 }
-cDNA_names[k] = strsplit(myFiles, "[.]")[[k]][1]
+cDNA_names[k] = strsplit(myFiles2, "[.]")[[k]][1]
 names(cDNA) = cDNA_names
 
 ########### Split START and STOP values ##########
@@ -56,6 +56,39 @@ for (org in seq(1, length(cDNA), by = 1)) {
     sort(as.numeric(strsplit(x, ";")[[1]])))
   #split by semicolon
 }
+
+# Read the predicted signal peptide cleavage sites ------------------------
+
+setwd(wd)
+myFiles3 <- list.files(pattern = "*signalp_positives.out")
+SP = list()
+SP_names = c()
+for (k in 1:length(myFiles3)) {
+  SP_names[k] = strsplit(myFiles3, "[.]")[[k]][1]
+  SP[[k]] = read.table(myFiles3[k], stringsAsFactors = F)
+  SP[[k]] = SP[[k]][,c(1,2,5)]
+}
+SP_names[k] = strsplit(myFiles3, "[.]")[[k]][1]
+names(SP) = SP_names
+
+# Read SAAR data within signal peptides -----------------------------------
+
+setwd(wd)
+myFiles4 <- list.files(pattern = "extracted_sigp_*")
+SAAR = list()
+SAAR_names = c()
+for (k in 1:length(myFiles4)) {
+  SAAR_names[k] = strsplit(myFiles4, "[.]")[[k]][1]
+  SAAR[[k]] = read.csv(myFiles4[k], stringsAsFactors = F,header = F)
+  SAAR[[k]] = SAAR[[k]][,-c(2:4)]
+  faultyRows=which(!grepl('ENS',SAAR[[k]][,1]))
+  if(length(faultyRows)>0){
+    SAAR[[k]] = SAAR[[k]][-c(which(!grepl('ENS',SAAR[[k]][,1]))),]
+  }
+}
+SAAR_names[k] = strsplit(myFiles4, "[.]")[[k]][1]
+names(SAAR) = SAAR_names
+
 
 # Save environment --------------------------------------------------------
 
