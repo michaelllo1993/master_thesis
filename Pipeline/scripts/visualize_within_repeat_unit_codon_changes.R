e9059555985e9da4e3e8f9586ff0fd2bfbc94fd0
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-03-12 16:41:41 -0400

    Finished up the visualization of codon changes within repeat units

--- Pipeline/scripts/visualize_within_repeat_unit_codon_changes.R
+++ Pipeline/scripts/visualize_within_repeat_unit_codon_changes.R
@@ -5,6 +5,7 @@ require(gplots)
 require(seqinr)
 require(Biostrings)
 require(fmsb)
+require(svglite)
 
 # Get the arguments -------------------------------------------------------
 
@@ -16,8 +17,6 @@ args = commandArgs(trailingOnly=TRUE)
 repeat_unit = args[length(args)]; args = args[-length(args)]
 #get the last argument and delete it
 OoI =  args[length(args)]; args = args[-length(args)]
-#get the last argument and delete it
-other_OoIs =  args[length(args)]; args = args[-length(args)]
 #all other arguments are files with data to be visualized
 codon_changes_files = args
 #infer the output directory from one input files
@@ -36,11 +35,15 @@ i=1
 
 for (file_name in codon_changes_files){
   #get the other organism name which matches the input file name - other organism of interest
-  other_OoI = names(which(sapply(other_OoIs, function(x) grepl(x,file_name))))
+  other_OoI = gsub(".csv","",file_name)
+  splitted=strsplit(other_OoI,"_")[[1]]
+  other_OoI=paste(splitted[seq(length(splitted)-1,length(splitted))],collapse = "_")
+  print(other_OoI)
   #get the name of the region of interest which matches to input file name 
   RoI = names(which(sapply(regions, function(x) grepl(x,file_name))))
   #construct the name of the output image
-  image_name = paste("/visualization_",OoI,"/",gsub(".csv",".svg",file_name),sep = "")
+  image_name = gsub(".csv",".svg",basename(file_name))
+  print(image_name)
   #read the data in
   occurrences=data.matrix(read.csv(file_name,header = T,sep = ",",row.names = all_codon_names))[,-1]
   #change 0 to NAs
@@ -48,14 +51,15 @@ for (file_name in codon_changes_files){
   #keep just the codons encoding the repeat unit
   within_repeat_unit = occurrences[unit_codons,]
   within_repeat_unit_list[[i]] = within_repeat_unit
-  #create image and seve it 
-  svg(filename=image_name,
+  setwd(paste(dir,"/visualization_",OoI,sep = ""))
+  #create image and save it 
+  svglite(file=image_name,
       width=40, 
       height=30, 
       pointsize=50)
   heatmap.2(dendrogram = "none",within_repeat_unit,cexRow=1.1,cexCol=1.1, Rowv = NA,Colv = NA,scale = "row", key = T, key.xtickfun = NULL,density.info = "none",trace = "none",symkey = F,col=redblue,na.color = "red", colsep = c(1,2,3,4,5,6),sepwidth = c(0.021, 0.021),srtCol=90, offsetRow=0, offsetCol=1,rowsep = seq(1,dim(occurrences)[1],1),xlab = paste("Codons in",OoI),ylab = paste("Codons in",other_OoI),cellnote = within_repeat_unit,notecol="black",notecex=1.5, main = paste("Codon changes within ",repeat_unit," in all ",RoI,"s",sep = ""),keysize = 1)
   dev.off()
-
+  setwd(wd)
   # Cohen's kappa estimation ------------------------------------------------
   if(any(is.na(within_repeat_unit))){
     within_repeat_unit[which(is.na(within_repeat_unit))] = 0
@@ -66,7 +70,7 @@ for (file_name in codon_changes_files){
   kappa_result = Kappa.test(to_kappa_est)
   string = paste(sep = ",",OoI, other_OoI,RoI,round(kappa_result$Result$estimate,digits = 2),paste(sep = "",round(kappa_result$Result$conf.int[1],digits = 2)," - ",round(kappa_result$Result$conf.int[2],digits = 2)),kappa_result$Result$p.value)
   if(file.exists(paste(dir,"/visualization_",OoI,"/kappa_results_",other_OoI,".csv",sep = ""))){
-    write(string,file = paste(dir,"/visualization_",OoI,"kappa_results_",other_OoI,".csv",sep = ""),append = T);
+    write(string,file = paste(dir,"/visualization_",OoI,"/kappa_results_",other_OoI,".csv",sep = ""),append = T);
   } else{
     header = paste(sep=",","higher_organism","lower_organism","region","Kappa estimate","CI","p-value")
     write(header,file = paste(dir,"/visualization_",OoI,"/kappa_results_",other_OoI,".csv",sep = ""),append = T);
