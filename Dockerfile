67569ba2255c3bdcfe8ec8edfbf8ee250b0d2309
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-03-19 22:15:11 -0400

    Fixed #12. Respective changes in the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:latest as OS
 
-MAINTAINER Michal Stolarczyk (mjs5kd@virginia.edu)
+MAINTAINER Michal Stolarczyk (stolarczyk.michal93@gmail.com)
 
 # Add cran R backport
 RUN apt-get -y update
@@ -12,30 +12,40 @@ RUN echo "deb https://mirrors.ebi.ac.uk/CRAN/bin/linux/ubuntu xenial/" >> /etc/a
 RUN apt-get -y update
 RUN apt-get -y dist-upgrade
 
+#Install system requirements for the software packages below
+RUN apt-get install -y build-essential libssl-dev libffi-dev python-dev libcurl4-openssl-dev libxml2-dev wget libx11-dev
+RUN apt -y install python3-pip
 
+#Install snakemake 
+RUN pip3 install snakemake
 
-
-
-
-
-
-
-#Install other R packages
+#Install R and R packages
+RUN apt install -y r-base
 RUN echo "r <- getOption('repos'); r['CRAN'] <- 'http://cran.us.r-project.org'; options(repos = r);" > ~/.Rprofile
-RUN Rscript -e "install.packages('ggplot2')"
-RUN Rscript -e "install.packages('markdown')"
-RUN Rscript -e "install.packages('igraph')"
-RUN Rscript -e "install.packages('dplyr')"
-RUN Rscript -e "install.packages('shiny')"
-RUN Rscript -e "install.packages('shinythemes')"
-RUN Rscript -e "install.packages('shinyBS')"
-RUN Rscript -e "install.packages('intergraph')"
-RUN Rscript -e "install.packages('visNetwork')"
-RUN Rscript -e "install.packages('xtable')"
-RUN Rscript -e "install.packages('sna')"
-RUN Rscript -e "install.packages('rPython')"
+RUN Rscript -e "install.packages('seqinr')"
+RUN Rscript -e "install.packages('RCurl')"
+RUN Rscript -e "install.packages('httr')"
+RUN Rscript -e "install.packages('XML')"
+RUN Rscript -e "install.packages('seqinr')"
+RUN Rscript -e "install.packages('ape')"
+RUN Rscript -e "source('https://bioconductor.org/biocLite.R');biocLite('Biostrings')"
+RUN Rscript -e "source('https://bioconductor.org/biocLite.R');biocLite('biomaRt')"
 
-
-
+#Install git
+RUN apt-get install -y git
 #Clone the git repository
-RUN git clone https://github.com/michalstolarczyk/master_thesis.git
+RUN git clone https://github.com/michalstolarczyk/SAARpipeline.git
+
+#copy the example data
+COPY Pipeline/data SAARpipeline/Pipeline/data
+
+#download and install EMBOSS software
+WORKDIR SAARpipeline/Pipeline
+RUN wget ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.6.0.tar.gz
+RUN gunzip EMBOSS-6.6.0.tar.gz 
+RUN tar xvf EMBOSS-6.6.0.tar
+WORKDIR EMBOSS-6.6.0
+RUN ./configure
+RUN make
+WORKDIR ..
+RUN rm EMBOSS-6.6.0.tar
--- Pipeline/scripts/calculate_mean_evolutionary_distances.R
+++ Pipeline/scripts/calculate_mean_evolutionary_distances.R
@@ -23,15 +23,17 @@ proteins = readRDS(paste(wd, "/data/readData/readData_proteins.rds", sep = ""))
 #read mapper with common orthologoues protein IDs
 mapper = as.matrix(read.csv(paste(OoI,"_ids_mapper_common.csv",sep = ""), header = T)[,-1])
 #get number of rows of the mapper 
+
 no_of_proteins = dim(mapper)[1]
 #prepare an empty list for final results
 list_distances = list()
+row_number=c()
 #main loop - 100 iteration = 100 random protein samples
 for (rep in seq_len(1000)) {
   #draw row number
-  row_number = sample(1:no_of_proteins, 1)
+  row_number[rep] = sample(1:no_of_proteins, 1)
   #get the orthologous IDs
-  ids = mapper[row_number,]
+  ids = mapper[row_number[rep],]
   #get the names of organisms associated with them
   ids_names = names(ids)
   
@@ -41,8 +43,8 @@ for (rep in seq_len(1000)) {
   while (i <= length(ids)) {
     number = (which(proteins[[ids_names[i]]]$ID == ids[i]))
     if (length(number) < 1) {
-      row_number = sample(1:no_of_proteins, 1)
-      ids = mapper[row_number,]
+      row_number[rep] = sample(1:no_of_proteins, 1)
+      ids = mapper[row_number[rep],]
       sequences = c()
       i = 1
     } else{
@@ -56,23 +58,28 @@ for (rep in seq_len(1000)) {
   sequences_converted = AAStringSet(sequences,use.names = T)
   alignment = msa(sequences_converted,method = "Muscle")
   list_distances[[rep]] = dist.alignment(msaConvert(alignment,type = "seqinr::alignment"))
+  print(rep)
 }
 
 #average the results obtained from all iterations
 summed = list_distances[[1]]
 for(e in seq_len(length(list_distances))){
-  summed = list_distances[[e]] + summed
+  if(any(is.nan(list_distances[[e]]))){
+    summed = summed
+  } else{
+    summed = list_distances[[e]] + summed  
+  }
 }
 
 averaged=summed/length(list_distances)
 
 #write the averaged distance matrix to CSV file
-write.csv(x = averaged,file = paste(wd,"/mean_pairwise_evolutionary_distances.csv",sep = ""),row.names = T,col.names = T)
+write.csv(x = as.matrix(averaged),file = paste(wd,"/mean_pairwise_evolutionary_distances.csv",sep = ""),row.names = T,col.names = T)
 #generate a phylogenetic tree based on averged pairwise distances 
 svglite(file="phylogenetic_tree.svg",
         width=40, 
         height=30, 
         pointsize=50)
-plot(as.phylo(hclust((averaged),method = "average")), cex = 1, label.offset = 0.01,main = "Phylogenetic tree; hierarchical clusetring using UPGMA metod",sub = 
+plot(as.phylo(hclust((averaged),method = "ward.D2")), cex = 1, label.offset = 0.01,main = "Phylogenetic tree; hierarchical clusetring using UPGMA metod",sub = 
        paste("Based on alignment of", rep ,"randomly selected protein sequences"),edge.width = 3)
 dev.off()
