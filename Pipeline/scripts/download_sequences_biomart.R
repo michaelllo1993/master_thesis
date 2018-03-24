7bdf14781ad5b062bb8ffff635d5dc50e8ab8416
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-03-24 15:44:01 -0400

    worked on header coposition and saving the c

--- Pipeline/scripts/download_sequences_biomart.R
+++ Pipeline/scripts/download_sequences_biomart.R
@@ -19,17 +19,21 @@ for(organism in seq_len(length(organisms))){
   # first table with CDNA start/end values
   transcripts <- getBM(values = genes$ensembl_gene_id, filters = "ensembl_gene_id", attributes = c("ensembl_transcript_id","ensembl_peptide_id","cdna_coding_start","cdna_coding_end"), mart = mart)
   # process the dataframe: collapse entries with the same transcript ID
-  transcripts_mod = transcripts %>% group_by(ensembl_transcript_id) %>% summarise(START = paste(cdna_coding_start,collapse = ","), STOP = paste(cdna_coding_end,collapse = ","))
+  transcripts_mod = transcripts %>% group_by(ensembl_transcript_id) %>% summarise(ensembl_peptide_id = unique(ensembl_peptide_id)[1],START = paste(cdna_coding_start,collapse = ","), STOP = paste(cdna_coding_end,collapse = ","))
   rm(transcripts)
   # second table with sequences
   transcripts2 <- getBM(values = genes$ensembl_gene_id, filters = "ensembl_gene_id", attributes = c("ensembl_transcript_id","cdna"),mart = mart)
   rm(mart,genes)
   # merge the data.frames
   merged = inner_join(transcripts2,transcripts_mod)
+  headers=c()
+  for(i in seq_len(nrow(merged))){
+    headers[i] = paste(merged$ensembl_transcript_id[i],merged$ensembl_peptide_id[i],gsub(pattern = ",",replacement = ";",merged$START[i]),gsub(pattern = ",",replacement = ";",merged$STOP[i]),sep = "|")
+  }
   rm(transcripts2,transcripts_mod)
   # assign to the output var
   cDNA[[organism]] = merged
-  rm(merged)
+  write.fasta(as.list(merged$cdna),names = headers,file.out = paste(wd,"/data/cDNAsequences/test/",orgs[organism],"_cDNA.txt",sep = ""),nbchar = 50)
 }
 names(cDNA) = orgs
 saveRDS(cDNA,paste(wd,"data/readData/readData_cDNA.rds",sep = ""))
@@ -44,7 +48,7 @@ for(organism in seq_len(length(organisms))){
   genes <- getBM(attributes = "ensembl_gene_id", mart = mart)
   # first table with CDNA start/end values
   peptides <- getBM(values = genes$ensembl_gene_id, filters = "ensembl_gene_id", attributes = c("ensembl_peptide_id","peptide"), mart = mart)
-  write.fasta(sequences = as.list(peptides$peptide),names = peptides$ensembl_peptide_id,as.string = T,file.out = paste(wd,"/data/proteinSequences/test/",orgs[organism],".txt",sep = ""))
+  write.fasta(sequences = as.list(peptides$peptide),names = peptides$ensembl_peptide_id,as.string = T,file.out = paste(wd,"/data/proteinSequences/",orgs[organism],".txt",sep = ""))
   # assign to the output var 
   protein[[organism]] = peptides
   rm(peptides,genes,mart)
