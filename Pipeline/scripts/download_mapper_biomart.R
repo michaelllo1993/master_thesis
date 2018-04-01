533ac0f5bc41cc898500938487ab84d6bf1cbc1c
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-04-01 12:13:09 -0400

    Added error handling for the Ensembl data retrival

--- Pipeline/scripts/download_mapper_biomart.R
+++ Pipeline/scripts/download_mapper_biomart.R
@@ -6,48 +6,79 @@ args = commandArgs(trailingOnly = TRUE)
 organisms_names = args
 orgs = args
 #convert orginal organisms names to ones used in the database
-orgs = as.vector(sapply(orgs, function(x) paste(s2c(strsplit(x,split = "_")[[1]][1])[1],strsplit(x,split = "_")[[1]][2],sep = "")))
-#get the organism of interest - first one 
+orgs = as.vector(sapply(orgs, function(x)
+  paste(
+    s2c(strsplit(x, split = "_")[[1]][1])[1], strsplit(x, split = "_")[[1]][2], sep = ""
+  )))
+#get the organism of interest - first one
 OoI = orgs[1]
-organisms= sort(orgs[-1])
+organisms = sort(orgs[-1])
 
 #set the biomart to be used
-mart_name <- useMart(biomart = "ensembl", paste(OoI,"_gene_ensembl",sep = ""),host = "useast.ensembl.org")
+mart_name <-
+  useMart(biomart = "ensembl",
+          paste(OoI, "_gene_ensembl", sep = ""),
+          host = "useast.ensembl.org")
 
 mappers = list()
 batch_size = 2
 #get the count of full orthologues dowlonloads to be done (which contain 3 ortohlogues)
-full_batches = length(organisms)%/%batch_size
-#perform org number of full downloads 
-for(org in seq_len(full_batches)){
+full_batches = length(organisms) %/% batch_size
+#perform org number of full downloads
+for (org in seq_len(full_batches)) {
   #get proper organism for each batch
-  start_element = org*batch_size-1
-  stop_element = org*batch_size
+  start_element = org * batch_size - 1
+  stop_element = org * batch_size
   #set the attributes
-  atrbts = append("ensembl_peptide_id",paste(organisms[start_element:stop_element],"_homolog_ensembl_peptide",sep = ""))
+  atrbts = append("ensembl_peptide_id",
+                  paste(organisms[start_element:stop_element], "_homolog_ensembl_peptide", sep = ""))
   #acquire the data
-  mappers[[org]] = as.matrix(getBM(
-    attributes = atrbts,
-    mart = mart_name
-  ))
+  tryCatch(
+    mappers[[org]] = as.matrix(getBM(
+      attributes = atrbts,
+      mart = mart_name
+    )),
+    error = function(error_message) {
+      message(
+        paste(
+          "The downloading of the mapper has failed due to server problems. Please run the rule once again.",
+          sep = ""
+        )
+      )
+      return(NA)
+    }
+  )
+  
   #save the data with non-empyt rows in first column (with IDs of organism of interest)
-  mappers[[org]] = mappers[[org]][which(mappers[[org]][,1] != ""),]
+  mappers[[org]] = mappers[[org]][which(mappers[[org]][, 1] != ""), ]
 }
-if(stop_element != length(organisms)){
+if (stop_element != length(organisms)) {
   #perform a non-full download for the organisms that left
-  leftover = organisms[(stop_element+1):length(organisms)]
-  atrbts = append("ensembl_peptide_id",paste(leftover,"_homolog_ensembl_peptide",sep = ""))
-  mappers[[org+1]] = as.matrix(getBM(
-    attributes = atrbts,
-    mart = mart_name
-  ))
-  mappers[[org+1]] = mappers[[org+1]][which(mappers[[org+1]][,1] != ""),]
+  leftover = organisms[(stop_element + 1):length(organisms)]
+  atrbts = append("ensembl_peptide_id",
+                  paste(leftover, "_homolog_ensembl_peptide", sep = ""))
+  tryCatch(
+    mappers[[org + 1]] = as.matrix(getBM(
+      attributes = atrbts,
+      mart = mart_name
+    )),
+    error = function(error_message) {
+      message(
+        paste(
+          "The downloading of the mapper has failed due to server problems. Please run the rule once again.",
+          sep = ""
+        )
+      )
+      return(NA)
+    }
+  )
+  mappers[[org + 1]] = mappers[[org + 1]][which(mappers[[org + 1]][, 1] != ""), ]
 }
 #define the function for mapper list preprocessing
-preprocess_mapper_list <- function(x){
-  x=x[which(!duplicated(x[,1])),]
-  rownames(x) = x[,1]
-  x=x[,-1]
+preprocess_mapper_list <- function(x) {
+  x = x[which(!duplicated(x[, 1])), ]
+  rownames(x) = x[, 1]
+  x = x[, -1]
   return(x)
 }
 
@@ -56,17 +87,21 @@ mappers = lapply(mappers, preprocess_mapper_list)
 #initialize the mapper list
 mapper = mappers[[1]]
 #merge all mappers together
-for(i in seq_len(length(mappers)-1)){
-  mapper = merge(mapper,mappers[[i+1]],by="row.names")
-  rownames(mapper) = mapper[,1]
-  mapper = mapper[,-1]
+for (i in seq_len(length(mappers) - 1)) {
+  mapper = merge(mapper, mappers[[i + 1]], by = "row.names")
+  rownames(mapper) = mapper[, 1]
+  mapper = mapper[, -1]
 }
 
-mapper = cbind(rownames(mapper),mapper)
+mapper = cbind(rownames(mapper), mapper)
 column_names = colnames(mapper)
 column_names[1] = "ensembl_peptide_id"
 colnames(mapper) = column_names
 #write mappers to the file
-write.table(mapper,file = paste(wd,"/data/orthoMappers/",organisms_names[1],".tsv",sep=""),sep = "\t",row.names = F)
-
-
+write.table(
+  mapper,
+  file = paste(wd, "/data/orthoMappers/", organisms_names[1], ".tsv", sep =
+                 ""),
+  sep = "\t",
+  row.names = F
+)
--- Pipeline/scripts/download_sequences_biomart.R
+++ Pipeline/scripts/download_sequences_biomart.R
@@ -4,44 +4,109 @@ library(dplyr)
 
 wd = getwd()
 args = commandArgs(trailingOnly = TRUE)
-organism_full=args
-organisms = paste(s2c(strsplit(organism_full,split = "_")[[1]][1])[1],strsplit(organism_full,split = "_")[[1]][2],sep = "")
+organism_full = args
+organisms = paste(s2c(strsplit(organism_full, split = "_")[[1]][1])[1], strsplit(organism_full, split = "_")[[1]][2], sep = "")
 
 # read cDNA sequences ----------------------------------------------------------
 
-cDNA=list()
-mart <- useEnsembl(biomart = "ensembl",dataset = paste(organisms,"_gene_ensembl",sep = ""),host = "useast.ensembl.org")
+cDNA = list()
+# mart <- useEnsembl(biomart = "ensembl",dataset = paste(organisms,"_gene_ensembl",sep = ""),host = "useast.ensembl.org")
+mart <-
+  useEnsembl(biomart = "ensembl",
+             dataset = paste(organisms, "_gene_ensembl", sep = ""))
 # vector of all gene IDs (only used to improve processing later)
 genes <- getBM(attributes = "ensembl_gene_id", mart = mart)
 # first table with CDNA start/end values
-transcripts <- getBM(values = genes$ensembl_gene_id, filters = "ensembl_gene_id", attributes = c("ensembl_transcript_id","ensembl_peptide_id","cdna_coding_start","cdna_coding_end"), mart = mart)
+tryCatch(
+  transcripts <-
+    getBM(
+      values = genes$ensembl_gene_id,
+      filters = "ensembl_gene_id",
+      attributes = c(
+        "ensembl_transcript_id",
+        "ensembl_peptide_id",
+        "cdna_coding_start",
+        "cdna_coding_end"
+      ),
+      mart = mart
+    ),
+  error = function(error_message) {
+    message(
+      paste(
+        "The downloading of the ",
+        organism_full,
+        " cDNA sequences has failed due to server problems. Please run the rule once again.",
+        sep = ""
+      )
+    )
+    return(NA)
+  }
+)
+
 # process the dataframe: collapse entries with the same transcript ID
-transcripts_mod = transcripts %>% group_by(ensembl_transcript_id) %>% summarise(ensembl_peptide_id = unique(ensembl_peptide_id)[1],START = paste(cdna_coding_start,collapse = ","), STOP = paste(cdna_coding_end,collapse = ","))
+transcripts_mod = transcripts %>% group_by(ensembl_transcript_id) %>% summarise(
+  ensembl_peptide_id = unique(ensembl_peptide_id)[1],
+  START = paste(cdna_coding_start, collapse = ","),
+  STOP = paste(cdna_coding_end, collapse = ",")
+)
 rm(transcripts)
 # second table with sequences
-transcripts2 <- getBM(values = genes$ensembl_gene_id, filters = "ensembl_gene_id", attributes = c("ensembl_transcript_id","cdna"),mart = mart)
-rm(mart,genes)
+tryCatch(
+  transcripts2 <-
+    getBM(
+      values = genes$ensembl_gene_id,
+      filters = "ensembl_gene_id",
+      attributes = c("ensembl_transcript_id", "cdna"),
+      mart = mart
+    ),
+  error = function(error_message) {
+    message(
+      paste(
+        "The downloading of the ",
+        organism_full,
+        " cDNA sequences has failed due to server problems. Please run the rule once again.",
+        sep = ""
+      )
+    )
+    return(NA)
+  }
+)
+rm(mart, genes)
 # merge the data.frames
-merged = inner_join(transcripts2,transcripts_mod)
+merged = inner_join(transcripts2, transcripts_mod)
 #delete the sequences that do not have the peptide counterpart
-merged = merged[-which(merged$ensembl_peptide_id == ""),]
+merged = merged[-which(merged$ensembl_peptide_id == ""), ]
 #rearrange columns order
-merged = merged[,c(1,3,4,5,2)]
+merged = merged[, c(1, 3, 4, 5, 2)]
 names_merged = names(merged)
-headers=c()
-for(i in seq_len(nrow(merged))){
-  starts=gsub(pattern = "NA,",replacement = "",merged$START[i])
+headers = c()
+for (i in seq_len(nrow(merged))) {
+  starts = gsub(pattern = "NA,", replacement = "", merged$START[i])
   merged$START[i] = starts
-  stops=gsub(pattern = "NA,",replacement = "",merged$STOP[i])
+  stops = gsub(pattern = "NA,", replacement = "", merged$STOP[i])
   merged$STOP[i] = stops
-  headers[i] = paste(merged$ensembl_transcript_id[i],merged$ensembl_peptide_id[i],gsub(pattern = ",",replacement = ";",starts),gsub(pattern = ",",replacement = ";",stops),sep = "|")
+  headers[i] = paste(
+    merged$ensembl_transcript_id[i],
+    merged$ensembl_peptide_id[i],
+    gsub(pattern = ",", replacement = ";", starts),
+    gsub(pattern = ",", replacement = ";", stops),
+    sep = "|"
+  )
 }
-rm(transcripts2,transcripts_mod)
+rm(transcripts2, transcripts_mod)
 # assign to the output var
 cDNA[[1]] = merged
-write.fasta(as.list(merged$cdna),names = headers,file.out = paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA.txt",sep = ""))
+write.fasta(
+  as.list(merged$cdna),
+  names = headers,
+  file.out = paste(wd, "/data/cDNAsequences/", organism_full, "_cDNA.txt", sep = "")
+)
 #Convert the .txr file to .csv format with a Perl script
-command=(paste("perl scripts/seq2csv_cDNA.pl",paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA.txt",sep = ""),sep=" "))
+command = (paste(
+  "perl scripts/seq2csv_cDNA.pl",
+  paste(wd, "/data/cDNAsequences/", organism_full, "_cDNA.txt", sep = ""),
+  sep = " "
+))
 system(command)
 #remove rows with 'NA'
 # command = paste("grep -Ev '\\|NA\\|'", paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA.csv",sep = ""), " > ", paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA_edited.csv",sep = ""), sep = "")
@@ -49,8 +114,11 @@ system(command)
 # system("mv ",paste(paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA_edited.csv",sep = ""),paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA.csv",sep = ""),sep=""))
 rm(cDNA)
 #read the .csv file
-cDNA = read.csv(paste(wd,"/data/cDNAsequences/",organism_full,"_cDNA.csv",sep = ""), stringsAsFactors = F)
-cDNA = cDNA[!(cDNA[,1])=="",]
+cDNA = read.csv(
+  paste(wd, "/data/cDNAsequences/", organism_full, "_cDNA.csv", sep = ""),
+  stringsAsFactors = F
+)
+cDNA = cDNA[!(cDNA[, 1]) == "", ]
 cDNA$START = lapply(cDNA$START, function(x)
   sort(as.numeric(strsplit(x, ";")[[1]])))
 #split by semicolon
@@ -61,25 +129,69 @@ names(cDNA) = names_merged
 cDNA = list(cDNA)
 names(cDNA) = organism_full
 #save for later use
-saveRDS(cDNA,paste(wd,"/data/readData/readData_cDNA_",organism_full,".rds",sep = ""))
+saveRDS(cDNA,
+        paste(
+          wd,
+          "/data/readData/readData_cDNA_",
+          organism_full,
+          ".rds",
+          sep = ""
+        ))
 
 
 # read protein sequences -------------------------------------------------------
 
-protein=list()
-mart <- useEnsembl(biomart = "ensembl",dataset = paste(organisms,"_gene_ensembl",sep = ""))
+protein = list()
+mart <-
+  useEnsembl(biomart = "ensembl",
+             dataset = paste(organisms, "_gene_ensembl", sep = ""))
 # vector of all gene IDs (only used to improve processing later)
 genes <- getBM(attributes = "ensembl_gene_id", mart = mart)
 # first table with CDNA start/end values
-peptides <- getBM(values = genes$ensembl_gene_id, filters = "ensembl_gene_id", attributes = c("ensembl_peptide_id","ensembl_transcript_id","peptide"), mart = mart)
-headers=c()
-for(i in seq_len(nrow(peptides))){
-  headers[i] = paste(peptides$ensembl_peptide_id[i],peptides$ensembl_transcript_id[i],sep = "|")
+tryCatch(
+  peptides <-
+    getBM(
+      values = genes$ensembl_gene_id,
+      filters = "ensembl_gene_id",
+      attributes = c("ensembl_peptide_id", "ensembl_transcript_id", "peptide"),
+      mart = mart
+    ),
+  error = function(error_message) {
+    message(
+      paste(
+        "The downloading of the ",
+        organism_full,
+        " protein sequences has failed due to server problems. Please run the rule once again.",
+        sep = ""
+      )
+    )
+    return(NA)
+  }
+)
+headers = c()
+for (i in seq_len(nrow(peptides))) {
+  headers[i] = paste(peptides$ensembl_peptide_id[i],
+                     peptides$ensembl_transcript_id[i],
+                     sep = "|")
 }
-write.fasta(sequences = as.list(peptides$peptide),names = headers,as.string = T,file.out = paste(wd,"/data/proteinSequences/",organism_full,".txt",sep = ""))
-# assign to the output var 
+write.fasta(
+  sequences = as.list(peptides$peptide),
+  names = headers,
+  as.string = T,
+  file.out = paste(wd, "/data/proteinSequences/", organism_full, ".txt", sep = "")
+)
+# assign to the output var
 protein[[1]] = peptides
-rm(peptides,genes,mart)
+rm(peptides, genes, mart)
 names(protein) = organism_full
 
-saveRDS(protein,paste(wd,"/data/readData/readData_proteins_",organism_full,".rds",sep = ""))
+saveRDS(
+  protein,
+  paste(
+    wd,
+    "/data/readData/readData_proteins_",
+    organism_full,
+    ".rds",
+    sep = ""
+  )
+)
